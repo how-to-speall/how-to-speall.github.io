b3982f5c9d542a8c31e5b501cd1e81c59de0e237
how-to-speall <ahoura313@gmail.com>
2025-02-13 12:52:47 -0800

    changed head to try and fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"></link>
+      <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
       </head>
       <body className={cn("min-h-screen bg-background font-sans antialiased", inter.className)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
