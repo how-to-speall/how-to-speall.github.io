e409d0cb0599aa5aab1b0ed9aad14af70611d495
how-to-speall <ahoura313@gmail.com>
2025-02-13 22:03:13 -0800

    cleaned up some of the extra json in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  // basePath: "/mini-portfolio",
-  // assetPrefix: "/mini-portfolio/out",
   images: {
     unoptimized: true,
   },
